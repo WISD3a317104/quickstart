ee526ca991313ae2affc036ebdd571594ae27f34
s3a317104 <31909567+s3a317104@users.noreply.github.com>
2017-11-27 10:26:34 +0800

    layout + task increase

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -27,7 +27,15 @@ Route::group(['middleware' => 'web'], function () {
      * 增加新的任務
      */
     Route::post('/task', function (Request $request) {
-        //
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:255',
+        ]);
+    
+        if ($validator->fails()) {
+            return redirect('/')
+                ->withInput()
+                ->withErrors($validator);
+        }
     });
 
     /**
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Laravel 快速入門 - 基本</title>
+        <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel Quickstart - Basic</title>
+
+    <!-- Fonts -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel='stylesheet' type='text/css'>
+    <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700" rel='stylesheet' type='text/css'>
+
+    <!-- Styles -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+        <!-- CSS 及 JavaScript -->
+    </head>
+
+    <body>
+        <div class="container">
+            <nav class="navbar navbar-default">
+                <!-- Navbar 內容 -->
+            </nav>
+        </div>
+
+        @yield('content')
+    </body>
+</html>
